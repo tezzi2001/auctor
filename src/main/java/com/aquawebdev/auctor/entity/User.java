bccb5b6e2844d09e259ea212046c3bf971944424
Bohdan <bohdan.bondarenko2001@gmail.com>
2020-07-28 20:04:00 +0300

    add required primary key to user_t table

--- src/main/java/com/aquawebdev/auctor/entity/User.java
+++ src/main/java/com/aquawebdev/auctor/entity/User.java
@@ -1,7 +1,15 @@
 package com.aquawebdev.auctor.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "user_t")
+@Data
 public class User {
+    @Id
+    private String login;
 }
