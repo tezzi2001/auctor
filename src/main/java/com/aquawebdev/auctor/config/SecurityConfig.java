af4b0825ffa3286634bcafb2198efa613018fd59
Bohdan <bohdan.bondarenko2001@gmail.com>
2020-07-30 13:11:35 +0300

    add security config for basic paths

---
+++ src/main/java/com/aquawebdev/auctor/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.aquawebdev.auctor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf()
+                    .disable()
+                .authorizeRequests()
+                    .antMatchers("/signUp").not().fullyAuthenticated()
+                    .antMatchers("/news/**", "/article/**").hasRole("USER")
+                    .antMatchers("/profile", "/create").hasRole("USER")
+                    .antMatchers("/", "/resetPassword").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .successForwardUrl("/article")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .permitAll()
+                    .logoutSuccessUrl("/");
+
+
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
