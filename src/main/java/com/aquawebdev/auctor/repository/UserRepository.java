0384ad8bae983b4aea0afbb83908573cbd75f3de
Bohdan <bohdan.bondarenko2001@gmail.com>
2020-08-02 18:31:13 +0300

    add method for finding user by login in user repository

--- src/main/java/com/aquawebdev/auctor/repository/UserRepository.java
+++ src/main/java/com/aquawebdev/auctor/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.aquawebdev.auctor.repository;
 
 import com.aquawebdev.auctor.entity.User;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, String> {
+    Optional<User> findByLogin(String login);
 }
