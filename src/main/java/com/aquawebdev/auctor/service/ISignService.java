e6100066fbd32e3c6b0c061fc2a2641266b1ed7e
Bohdan <bohdan.bondarenko2001@gmail.com>
2020-08-02 18:35:42 +0300

    add service for registration, authentication and resetting password

---
+++ src/main/java/com/aquawebdev/auctor/service/ISignService.java
@@ -0,0 +1,9 @@
+package com.aquawebdev.auctor.service;
+
+import com.aquawebdev.auctor.entity.User;
+
+public interface ISignService {
+    boolean signIn(String login, String password);
+    boolean signUp(User user);
+    boolean resetPassword(String email);
+}
