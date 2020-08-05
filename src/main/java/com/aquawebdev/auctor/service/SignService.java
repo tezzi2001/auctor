a1f0aa3b445a50314de27da0ed074bca831abaac
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-08-06 02:44:01 +0300

    SignSerciveImpl now provides interface for security persistence

--- src/main/java/com/aquawebdev/auctor/service/SignService.java
+++ src/main/java/com/aquawebdev/auctor/service/SignService.java
@@ -1,8 +1,9 @@
 package com.aquawebdev.auctor.service;
 
 import com.aquawebdev.auctor.entity.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
-public interface SignService {
+public interface SignService extends UserDetailsService {
     boolean signIn(String login, String password);
     boolean signUp(User user);
     void resetPassword(String email);
