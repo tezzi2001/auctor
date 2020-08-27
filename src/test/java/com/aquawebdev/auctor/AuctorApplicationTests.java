9938529089f690d95d20a6d3ab5aa0939ad52f0d
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-08-27 17:45:08 +0300

    add some demo tests

--- src/test/java/com/aquawebdev/auctor/AuctorApplicationTests.java
+++ src/test/java/com/aquawebdev/auctor/AuctorApplicationTests.java
@@ -1,19 +1,46 @@
 package com.aquawebdev.auctor;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
-import org.springframework.test.context.ActiveProfiles;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AuctorApplicationTests {
-    @Value("${spring.profiles.active}")
-    private String activeProfile;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
-        System.out.println("active profile: " + activeProfile);
+    void signUp_getUserAttributeIsOk() throws Exception {
+        mockMvc.perform(get("/signUp"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("user", allOf(
+                        hasProperty("userId", is(0L)),
+                        hasProperty("login", nullValue()),
+                        hasProperty("name", nullValue()),
+                        hasProperty("surname", nullValue()),
+                        hasProperty("email", nullValue()),
+                        hasProperty("password", nullValue()),
+                        hasProperty("photo", nullValue()),
+                        hasProperty("news", nullValue()),
+                        hasProperty("articles", nullValue()),
+                        hasProperty("roles", nullValue())
+                        )
+                ));
+    }
+
+    @Test
+    @WithMockUser(username = "u", password = "p", roles = {"USER", "ADMIN"})
+    void securityTestAbility() throws Exception {
+        mockMvc.perform(get("/signUp"))
+                .andExpect(status().isForbidden());
     }
 
 }
