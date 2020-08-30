48822922fea4ad2c9d50848bcb2ba6e559c38494
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2020-08-30 22:58:56 +0300

    add validation for sign up form

--- src/main/java/com/aquawebdev/auctor/controller/SignController.java
+++ src/main/java/com/aquawebdev/auctor/controller/SignController.java
@@ -5,9 +5,11 @@ import com.aquawebdev.auctor.service.SignService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.Valid;
 
 @Controller
 @AllArgsConstructor
@@ -21,7 +23,11 @@ public class SignController {
     }
 
     @PostMapping("/signUp")
-    public String signUp(@ModelAttribute("user") User user) {
+    public String signUp(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("errors", bindingResult.getAllErrors());
+            return "signUp";
+        }
         if (!signService.signUp(user)) {
             return "signUp";
         }
